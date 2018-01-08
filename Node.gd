82d045079216c077a0dcc96e476b11b5d1ee42cb
Chris <chrisneilbrown@gmail.com>
2018-01-08 16:37:14 +1100

    better rotation

--- Node.gd
+++ Node.gd
@@ -9,16 +9,18 @@ func _ready():
 	pass
 
 func _process(delta):
-	var y = deg2rad($Tank.rotation_degrees.y)
+	pass
+	
+func _physics_process(delta):
+	var y = deg2rad($Tank/mesh.rotation_degrees.y)
 	if Input.is_action_pressed("ui_up"):
 		$Tank.apply_impulse(Vector3(),Vector3(0,0,-1).rotated(Vector3(0,1,0),y))
 	if Input.is_action_pressed("ui_down"):
 		$Tank.apply_impulse(Vector3(),Vector3(0,0,1).rotated(Vector3(0,1,0),y))
 	if Input.is_action_pressed("ui_right"):
-		$Tank.rotate(Vector3(0,1,0),-1*delta)
+		$Tank/mesh.rotate(Vector3(0,1,0),-1*delta)
 	if Input.is_action_pressed("ui_left"):
-		
-		$Tank.rotate(Vector3(0,1,0),1*delta)
+		$Tank/mesh.rotate(Vector3(0,1,0),1*delta)
 	if Input.is_action_pressed("q"):
 		$Tank/mesh/turret.rotate_y(1.5*delta)	
 	if Input.is_action_pressed("e"):
